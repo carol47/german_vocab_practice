1d04337c2224ac594b013ab91f70043fb8b84de7
carol47 <carol.anasalles18@gmail.com>
2020-06-25 16:18:06 +0200

    Small changes

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -8,23 +8,32 @@ export default function HomePage() {
 
     return (
         <div css={css`
+
             margin-top: 64px;
-            width: 100%;
+            width: 100vw;
             height: 100%;
-            
             display: flex;
-            flex-direction: row;
+            flex-flow: row wrap;
+
             justify-content: space-evenly;
+
+            @media (max-width: 480px) {
+                margin-top: 32px;           
+            }
+
         `}>
             <div css={css`
-                width: 100%;
+                width: 40%;
                 height: 100%;
                 display: flex;
                 flex-direction: column;
                 
-        
                 
-                width: 40%;
+                @media (max-width: 480px) {
+                    width: 80%;
+                }
+                
+                
                 * {
                 margin: 10px;
                 }
@@ -33,13 +42,21 @@ export default function HomePage() {
                     font-family: "Georgia, serif";
                     font-size: 250%;
                     align-self: center;
+
+                    @media (max-width: 480px) {
+                        font-size: 150%;   
+                    }
                 `}>A language-learning experience</div>
-                <p>Welcome. German Vocabulary Learning is an indie tool to help memorize german vocabulary. It's on constant development, so remember to check it out regularly. To start, click on the 'Begin' link on the navigation bar. </p>
+                <p>Welcome. German Vocabulary Trainer is an indie tool to help memorize german vocabulary. It's on constant development, so remember to check it out regularly. To start, click on the 'Begin' link on the navigation bar. </p>
                 <p>Hint: you can use keys 1-4 to answer the questions.</p>
             </div>
             <div css={css`
                 width: 25%;
                 margin-top: 32px;
+
+                @media (max-width: 480px) {
+                    display: none;
+                }
             `}>
                 <img src={booksJpg} alt="A pile of books" css={css`max-width: 256px;`}/>
             </div>
